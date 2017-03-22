416e2e6c46e721a99a6503165b30e40be45e0539
Mathieu PATUREL <australie.p@gmail.com>
2017-03-22 16:43:51 +1100

    :tada: working :smile:

---
+++ WindowSettings.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+import sublime_plugin
+
+class WindowSetting(sublime_plugin.EventListener):
+
+    def on_activated(self, view):
+        window = view.window()
+        settings = view.settings()
+
+        show_tabs = settings.get('show_tabs')
+        show_minimap = settings.get('show_minimap')
+        show_status_bar = settings.get('show_status_bar')
+        show_sidebar = settings.get('show_sidebar')
+        show_menus = settings.get('show_menus')
+
+        if show_tabs is not None:
+            window.set_tabs_visible(show_tabs)
+        if show_minimap is not None:
+            window.set_minimap_visible(show_minimap)
+        if show_status_bar is not None:
+            window.set_status_bar_visible(show_status_bar)
+        if show_sidebar is not None:
+            window.set_sidebar_visible(show_sidebar)
+        if show_menus is not None:
+            window.set_menu_visible(show_menus)
